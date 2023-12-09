e1213004f34dc429421355530e0d8403df6ee605
ChoiHyeonSik <84433133+chs8003@users.noreply.github.com>
2023-12-09 21:27:27 +0900

    [level unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+
+        int i = 0;
+        while (n_str.charAt(i) == '0') {
+            i++;
+        }
+        answer = n_str.substring(i);
+
+        return answer;
+    }
+}
